7945fec4abfcfe9f845f91c8f4b64adf6b3c7cdd
sivadarsana-unni <85471239+sivadarsana-unni@users.noreply.github.com>
2021-06-07 11:45:58 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Note() {
+  return(
+    <div>
+      <h1> Javascript and React.js</h1>
+      <p>Amazing Bootcamp</p>
+    </div>
+  );
+}
+export default App;
